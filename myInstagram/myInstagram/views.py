3a86d5d51a5d839808984c87e563e7eee810def3
lunerave <lunerave07@naver.com>
2024-06-16 21:52:17 +0900

    Code Implementation : init home screen by using bootstrap starter template

--- myInstagram/myInstagram/views.py
+++ myInstagram/myInstagram/views.py
@@ -6,3 +6,7 @@ class Sub(APIView):
     def get(self, request):
         print("get 호출")
         return render(request, "myInstagram/main.html")
+
+    def post(self, request):
+        print("post 호출")
+        return render(request, "myInstagram/main.html")
