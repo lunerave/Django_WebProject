6b53e3ab4171dd8f9077afebecc4a9386052f35c
lunerave <lunerave07@naver.com>
2024-06-21 16:23:18 +0900

    Code Implementation : create DB with dummy data, integrate db and feed screen

--- myInstagram/content/views.py
+++ myInstagram/content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+
+        return render(request, "myInstagram/main.html", context=dict(feeds=feed_list))
