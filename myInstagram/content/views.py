5d69a624855862b4836007f69844bdd9b6ca1df1
lunerave <lunerave07@naver.com>
2024-06-24 17:10:42 +0900

    Code Implementation: feed upload in Django server and save data in DB; save feed image in Django project server

--- myInstagram/content/views.py
+++ myInstagram/content/views.py
@@ -1,7 +1,11 @@
+from uuid import uuid4
+
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
-
+import os
+from myInstagram.settings import MEDIA_ROOT
 
 # Create your views here.
 class Main(APIView):
@@ -9,3 +13,24 @@ class Main(APIView):
         feed_list = Feed.objects.all().order_by('-id')
 
         return render(request, "myInstagram/main.html", context=dict(feeds=feed_list))
+
+class UploadFeed(APIView):
+    def post(self, request):
+        file = request.FILES['file']
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        # 실제로 파일을 저장하는 코드
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+
+        image = uuid_name
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+
+        return Response(status=200)
