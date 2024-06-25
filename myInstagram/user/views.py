732c10edfef158d4dab16e68ab4a78201cf4152b
lunerave <lunerave07@naver.com>
2024-06-25 16:41:14 +0900

    Code Implementation: implement join API

--- myInstagram/user/views.py
+++ myInstagram/user/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
+from django.contrib.auth.hashers import make_password
 
 
 # Create your views here.
 class Join(APIView):
     def get(self, request):
         return render(request, 'user/join.html')
+
+    def post(self, request):
+        email = request.data.get('email', None)
+        nickname = request.data.get('nickname', None)
+        name = request.data.get('name', None)
+        password = request.data.get('password', None)
+
+        User.objects.create(email=email, nickname=nickname, name=name, password=make_password(password), profile_img='default_profile.jpg')
+
+        return Response(status=200)
 
 
 class Login(APIView):
