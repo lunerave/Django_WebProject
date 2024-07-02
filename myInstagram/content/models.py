913287ecf5e58fa48e446e5ce2f3c3741803a93c
lunerave <lunerave07@naver.com>
2024-07-02 10:15:15 +0900

    Code Implementation: add user custom model; Like, Reply, Bookmark

--- myInstagram/content/models.py
+++ myInstagram/content/models.py
@@ -7,3 +7,24 @@ class Feed(models.Model):
     image = models.TextField()          # 피드 이미지
     email = models.EmailField(default='')         # 글쓴이
     like_count = models.IntegerField()     # 좋아요 수
+
+
+class Like(models.Model):
+    content_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_like = models.BooleanField(default=True)
+
+
+class Reply(models.Model):
+    content_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
+
+
+class Bookmark(models.Model):
+    content_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_marked = models.BooleanField(default=True)
+
+
+
